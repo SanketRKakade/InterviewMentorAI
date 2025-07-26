bf162ea66b13860bdc9bf1fe01451a2697681c7e
SanketRKakade <sanketrkakade@gmail.com>
2025-07-26 16:31:26 +0530

    updated index.ts

--- config/database/index.ts
+++ config/database/index.ts
@@ -1,8 +1,14 @@
-import { neon, HTTPTransactionOptions } from '@neondatabase/serverless';
+'use server';
+
+import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const URL = process.env.DRIZZLE_DATABASE_URL as string;
+const URL = process.env.DRIZZLE_DATABASE_URL;
+
+if (!URL) {
+  throw new Error('⛔ DRIZZLE_DATABASE_URL is missing from environment');
+}
 
 const sql = neon(URL);
-    export const db = drizzle(sql,{schema});
+export const db = drizzle(sql, { schema });
