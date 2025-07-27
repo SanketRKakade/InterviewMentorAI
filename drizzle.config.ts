41d9a7878e56c13365a26a3304316c4cc32ee942
SanketRKakade <sanketrkakade@gmail.com>
2025-07-27 12:23:41 +0530

    Updated project with latest changes

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
+
+const URL = process.env.DRIZZLE_DATABASE_URL as string;
+
+export default defineConfig({
+  schema: "./config/database/schema.ts",
+  out: "./drizzle",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: "",
+  }
+});
